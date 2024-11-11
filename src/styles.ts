551382acce7c15dafbdface31753713734792eed
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-11-11 18:26:00 -0300

    first commit

---
+++ src/styles.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const colors = {
+  rosePink: '#E66767',
+  palePeach: '#FFEBD9',
+  white: '#FFFFFF',
+  goldenYellow: '#FFB930',
+  creamyWhite: '#FFF8F2'
+}
+
+export const GlobalCSS = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    text-decoration: none;
+  }
+
+  body {
+    background-color: ${colors.creamyWhite};
+    color:${colors.rosePink};
+    font-family:Roboto, sans-serif;
+    text-decoration: none;
+    list-style-type: none;
+  }
+  .container {
+  max-width: 100%;
+  width: 100%;
+  margin: 0 auto;
+}
+
+`
